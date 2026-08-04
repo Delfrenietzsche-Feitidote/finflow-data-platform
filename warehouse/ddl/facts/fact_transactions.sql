9202fce0aa1d765d585470e63b983f370eeef403
Giceline Delfrenietzsche <143403373+Delfrenietzsche-Feitidote@users.noreply.github.com>
2026-08-04 23:32:27 +0700

    Update fact_transactions.sql

--- warehouse/ddl/facts/fact_transactions.sql
+++ warehouse/ddl/facts/fact_transactions.sql
@@ -1,1 +1,46 @@
+-- ==========================================================
+-- Table: warehouse.fact_transactions
+-- Layer: Analytics Warehouse
+-- Purpose:
+-- Stores transaction attributes for analytical reporting.
+--
+-- Grain:
+-- One row represents one completed financial transaction.
+--
+-- Author: Alize
+-- ==========================================================
+
+CREATE TABLE warehouse.dim_payment_method (
+
+    transaction_key BIGINT PRIMARY KEY,
+
+    transaction_id VARCHAR(50) NOT NULL,
+
+    FOREIGN KEY (customer_key) REFERENCES warehouse.dim_customer(customer_key),
+
+    FOREIGN KEY (merchant_key) REFERENCES warehouse.dim_customer(merchant_key),
+
+    FOREIGN KEY (date_key) REFERENCES warehouse.dim_customer(date_key),
+
+    FOREIGN KEY (currency_key) REFERENCES warehouse.dim_customer(currency_key),
+
+    FOREIGN KEY (payment_method_key) REFERENCES warehouse.dim_customer(payment_method_key),
+
+    transaction_timestamp DATE NOT NULL,
+
+    transaction_amount DECIMAL(10,2) NOT NULL,
+
+    transaction_fee DECIMAL(10,2) NOT NULL,
+
+    exchange_rate DECIMAL(10,2) NOT NULL,
+
+    amount_usd DECIMAL(10,2) NOT NULL
+
+    batch_id VARCHAR(50) NOT NULL,
+
+    source_system VARCHAR(100),
+
+    load_timestamp DATE NOT NULL
+
+);
 
