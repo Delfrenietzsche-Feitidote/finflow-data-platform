45f51478a9dce45cb2d5b0427ebeef8dd907c343
Mikan_Fumino <mikanefumino@gmail.com>
2026-08-10 16:05:22 +0700

    feat: add database and pipeline settings

--- src/finflow/common/config.py
+++ src/finflow/common/config.py
@@ -1,5 +1,6 @@
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from datetime import date
 
 
 class DatabaseSettings(BaseSettings):
@@ -13,8 +14,25 @@ class DatabaseSettings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env",
         env_prefix="DB_",
-        case_sensitive=False,   
+        case_sensitive=False,
+        extra="ignore",
+    )
+
+
+class PipelineSettings(BaseSettings):
+    batch_date: date
+    batch_size: int
+    max_retries: int
+    retry_delay: int
+    source_system: str
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_prefix="PIPELINE_",
+        case_sensitive=False,
+        extra="ignore",
     )
 
 
 db_settings = DatabaseSettings()
+pipeline_settings = PipelineSettings()
